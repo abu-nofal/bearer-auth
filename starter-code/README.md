3390bd947ee2a66c2a7bf80dac5432fa04be42c6
abu-nofal <anofal719@gmail.com>
2021-08-16 21:57:41 +0300

    lab07

---
+++ starter-code/README.md
@@ -0,0 +1,36 @@
+# bearer-auth
+
+
+## Author: ahmad nofal
+- [tests report](https://github.com/abu-nofal/bearer-auth/actions)
+- [front-end]()
+## Setup
+`.env` requirements
+- PORT - [8080] ||[8000]
+
+`npm start`
+- Endpoint: /signup
+- Endpoint: /signin
+- Returns Object
+
+```
+{
+  "username":"name provided"
+},
+{
+    "username":"ahmad",
+    "password":"test123"
+
+}
+```
+## Tests
+- Unit Tests: npm run test
+- Lint Tests: npm run lint
+UML
+
+![](./imges/lab05.png)
+
+
+## link for deploy 
+
+- [for heroku]()
---
+++ starter-code/src/auth/middleware/basic.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const base64 = require('base-64');
+const { users } = require('../models/index.js')
+
+module.exports = async (req, res, next) => {
+
+  if (!req.headers.authorization) { return _authError(); }
+
+  let basic = req.headers.authorization.split(' ').pop();
+  let decoded=base64.decode(basic);
+  let [username, pass] = decoded.split(':');
+
+
+  // let basicHeaderParts = req.headers.authorization.split(' '); // ['Basic', encoded(username:password)]
+  // let encoded = basicHeaderParts.pop();
+  // let decoded = base64.decode(encoded); // username:password
+  // let [username, pass] = decoded.split(":"); // rawan test@1234
+
+  try {
+    req.user = await users.authenticateBasic(username, pass)
+    next();
+  } catch (e) {
+    res.status(403).send('Invalid Login');
+  }
+
+}
+
